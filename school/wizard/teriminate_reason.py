8b4c95b6a460fdfd026eae3a814c71de80ee0680
Freni Patel <freni.p.serpentcs@gmail.com>
2020-07-06 15:40:36 +0530

    [ADD][13]Added school

--- school/wizard/teriminate_reason.py
+++ school/wizard/teriminate_reason.py
@@ -1,29 +1,37 @@
 # See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields, api
+from odoo import fields, models
 
 
 class TerminateReason(models.TransientModel):
+    """Defining TransientModel to terminate reason."""
+
     _name = "terminate.reason"
     _description = "Terminate Reason"
 
-    reason = fields.Text('Reason')
+    reason = fields.Text("Reason")
 
-    @api.multi
     def save_terminate(self):
-        '''Method to terminate student and change state to terminate'''
-        self.env['student.student'
-                 ].browse(self._context.get('active_id')
-                          ).write({'state': 'terminate',
-                                   'terminate_reason': self.reason,
-                                   'active': False})
-        student_obj = self.env['student.student'].\
-            browse(self._context.get('active_id'))
+        """Method to terminate student and change state to terminate."""
+        self.env["student.student"].browse(
+            self._context.get("active_id")
+        ).write(
+            {
+                "state": "terminate",
+                "terminate_reason": self.reason,
+                "active": False,
+            }
+        )
+        student_obj = self.env["student.student"].browse(
+            self._context.get("active_id")
+        )
         student_obj.standard_id._compute_total_student()
-        user = self.env['res.users'].\
-            search([('id', '=', student_obj.user_id.id)])
-        student_reminder = self.env['student.reminder'].\
-            search([('stu_id', '=', student_obj.id)])
+        user = self.env["res.users"].search(
+            [("id", "=", student_obj.user_id.id)]
+        )
+        student_reminder = self.env["student.reminder"].search(
+            [("stu_id", "=", student_obj.id)]
+        )
         for rec in student_reminder:
             rec.active = False
         if user:
